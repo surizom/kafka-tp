dffd59ceafdb0bd1da842c84195ff658f9f71069
Moaad Fattali <f.moaad@gmail.com>
2021-03-20 08:59:59 +0100

    organize imports

--- src/main/java/if4030/kafka/WordTagger.java
+++ src/main/java/if4030/kafka/WordTagger.java
@@ -1,18 +1,15 @@
 package if4030.kafka;
 
 import java.io.BufferedReader;
-import java.io.Console;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URISyntaxException;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
-import java.util.Scanner;
 import java.util.concurrent.CountDownLatch;
+
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
