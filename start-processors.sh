22b20887914aec4e133e8feceb05b77d655da395
Moaad Fattali <f.moaad@gmail.com>
2021-03-20 08:58:56 +0100

    delete unused shell script

--- start-processors.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-java -cp target/tp-kafka-0.0.1-SNAPSHOT.jar if4030.kafka.WordCounter
-java -cp target/tp-kafka-0.0.1-SNAPSHOT.jar if4030.kafka.WordTagger
-java -cp target/tp-kafka-0.0.1-SNAPSHOT.jar if4030.kafka.ClassificationPrinter
